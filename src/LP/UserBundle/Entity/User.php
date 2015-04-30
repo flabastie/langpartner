4e6d508a25e3a1ebcadf14ba41d9c93b69d9bc90
flabastie <flabastie@hotmail.com>
2015-04-30 09:34:22 +0200

    install UserBundle

---
+++ src/LP/UserBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LP\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="lp_user")
+ * @ORM\Entity(repositoryClass="LP\UserBundle\Entity\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
