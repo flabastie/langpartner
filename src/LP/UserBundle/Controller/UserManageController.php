e780bca0e897d2e4b52cc6c1fc86eab45c254eea
flabastie <flabastie@hotmail.com>
2015-06-10 16:15:06 +0200

    Create user from dashboard

--- src/LP/UserBundle/Controller/UserManageController.php
+++ src/LP/UserBundle/Controller/UserManageController.php
@@ -155,6 +155,123 @@ class UserManageController extends Controller
 
 
 
+/* ------------------------------------------------------------------------------------------------------
+ *      fonction createUserAction
+ * ---------------------------------------------------------------------------------------------------- */
+
+  public function createUserAction(Request $request)
+  {
+
+    // creation user
+    $userToCreate  = new User();
+
+    $form = $this->get('form.factory')->create(new UserFullType(), $userToCreate);
+
+    // form treatment  ------------------------------------------------------------------------------------
+
+    if ($request->isMethod('POST')) 
+    {
+        $form ->bind($request);
+        $data = $form->getData();
+        $valid = false;
+
+      if (isset($_POST)) 
+      {
+
+        // username
+        if (isset($_POST['username']) && !empty($_POST['username'])) 
+        {
+          $userToCreate->setUsername($_POST['username']);
+          $valid = true;
+        }
+        else{
+              $valid = false;
+            }  
+
+        // useremail
+        if (isset($_POST['useremail']) && !empty($_POST['useremail'])
+        && preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $_POST['useremail']))
+        {
+          $userToCreate->setUseremail($_POST['useremail']);
+          $valid = true;
+        }
+        else{
+              $valid = false;
+            } 
+
+        // password
+        if (isset($_POST['password']) && !empty($_POST['password'])) 
+        {
+          $userToCreate->setPassword($_POST['password']);
+          $valid = true;
+        }
+        else{
+              $valid = false;
+            } 
+
+        // rolesCheckbox
+        if (isset($_POST['role_radio']) && !empty($_POST['role_radio'])) 
+        {
+          $userToCreate->setRoles(array($_POST['role_radio']));
+          $valid = true;
+        }
+        else{
+              $valid = false;
+            } 
+      }
+
+      if ($valid ) 
+      {
+
+        // verifications --------------------------------------------------------------------------------
+
+        // recup users
+        $users  = $this   ->getDoctrine()
+                          ->getManager()
+                          ->getRepository('LPUserBundle:User')
+                          ->findAll();
+
+        // loop users verif doublons
+        foreach ($users as $user) 
+        {
+          if ($user->getUsername() === $userToCreate->getUsername()) {
+            $request->getSession()->getFlashBag()->add('info', 'User name already used.');
+            return $this->redirect($this->generateUrl('lp_user_list'));
+          }
+          if ($user->getUseremail() === $userToCreate->getUseremail()) {
+            $request->getSession()->getFlashBag()->add('info', 'User email already used');
+            return $this->redirect($this->generateUrl('lp_user_list'));
+          }
+        }
+
+        // flush userTo --------------------------------------------------------------------------------
+
+        $activation = md5(uniqid(rand(), true));      // activation code
+        $userToCreate ->setAuth($activation);  
+        $userToCreate ->setSalt("");                    // salt
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($userToCreate);
+        $em->flush();
+
+        $request->getSession()->getFlashBag()->add('info', 'User well created.');
+        //return $this->redirect($this->generateUrl('lp_user_list'));
+      }
+      else{
+        $request->getSession()->getFlashBag()->add('info', 'User form error !');
+        return $this->redirect($this->generateUrl('lp_user_list'));
+      }
+
+    }
+
+    // rendering
+    return $this->render('LPUserBundle:Registration:create-user.html.twig', array(
+      'form'        => $form->createView()
+    ));
+
+  }
+
+
 }
 
 
